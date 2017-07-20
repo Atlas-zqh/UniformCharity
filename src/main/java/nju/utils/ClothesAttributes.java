96e464d030f1d1514d3cc6602983bee02cfb0792
151250206 <151250206@smail.nju.edu.cn>
2017-07-20 09:15:48 +0800

    <feature> <service> : <增加衣物分页>

--- src/main/java/nju/utils/ClothesAttributes.java
+++ src/main/java/nju/utils/ClothesAttributes.java
@@ -4,5 +4,5 @@ package nju.utils;
  * Created by keenan on 17/07/2017.
  */
 public enum ClothesAttributes {
-    SchoolName, Size, Type, Gender, Donor, Status;
+    schoolName, size, type, gender, donor, status;
 }
---
+++ src/main/java/nju/utils/ClothesQueryHelper.java
@@ -0,0 +1,63 @@
+package nju.utils;
+
+public class ClothesQueryHelper {
+    private String schoolName;
+
+    private String size;
+
+    private String type;
+
+    private String gender;
+
+    private String donor;
+
+    private String status;
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDonor() {
+        return donor;
+    }
+
+    public void setDonor(String donor) {
+        this.donor = donor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
