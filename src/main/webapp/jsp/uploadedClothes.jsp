e0da792b8a2d8126d2788f65077eddb1b7170bf8
Island0214 <549378813@qq.com>
2017-08-11 19:59:44 +0800

    <feat> <ui> : <捐衣页面已捐区域初始化>

--- src/main/webapp/jsp/uploadedClothes.jsp
+++ src/main/webapp/jsp/uploadedClothes.jsp
@@ -16,42 +16,51 @@
     <link rel="stylesheet" type="text/css" href="../static/css/theme.css">
     <link rel="stylesheet" type="text/css" href="../static/css/uploadedClothes.css">
     <script type="text/javascript" src="../static/js/jquery-3.2.1.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            getAllClothes();
+        });
+    </script>
 </head>
-<body style="border: thick solid #C9D7DA; border-bottom: none; border-left: none; background-color: #DFEDF0;">
-<h1 style="font-size: 5vw;     text-shadow: -0.4vw 0.4vw 0 #7A6420;">已捐衣物</h1>
-<table class="table-content">
-    <tbody class="table-content-title">
-    <tr>
-        <td style="width: 30%">
-            <a href="">123435345645</a>
-        </td>
-        <td style="width: 20%">
-            款式
-        </td>
-        <td style="width: 20%">
-            尺寸
-        </td>
-        <td style="width: 20%">
-            状态
-        </td>
-    </tr>
-    </tbody>
-    <tbody class="table-content-body">
-    <tr>
-        <th style="width: 50%;">
-            <div class="imageBox">
-                <img src="../static/images/mainBG2.png" class="largeImage"/>
-        <th style="width: 20%">
-            夏季短袖
-        </th>
-        <th style="width: 20%">
-            130
-        </th>
-        <th style="width: 20%">
-            未卖出
-        </th>
-    </tr>
-    </tbody>
-</table>
+<body style="border: thick solid #C9D7DA; border-bottom: none; border-left: none; background-color: #DFEDF0;" id="body">
+<h1 style="font-size: 5vw; text-shadow: -0.4vw 0.4vw 0 #7A6420;">已捐衣物</h1>
+
+<h4 style="font-size: 4vw; text-shadow: -0.3vw 0.3vw 0 #315770; position: absolute; top: 40%; width: 100%; text-align: center" id="noClothesLabel">暂无已捐衣物</h4>
+<%--<table class="table-content">--%>
+    <%--<tbody class="table-content-title">--%>
+    <%--<tr>--%>
+        <%--<td style="width: 30%">--%>
+            <%--<a href="">123435345645</a>--%>
+        <%--</td>--%>
+        <%--<td style="width: 20%">--%>
+            <%--款式--%>
+        <%--</td>--%>
+        <%--<td style="width: 20%">--%>
+            <%--尺寸--%>
+        <%--</td>--%>
+        <%--<td style="width: 20%">--%>
+            <%--状态--%>
+        <%--</td>--%>
+    <%--</tr>--%>
+    <%--</tbody>--%>
+    <%--<tbody class="table-content-body">--%>
+    <%--<tr>--%>
+        <%--<th style="width: 50%;">--%>
+            <%--<div class="imageBox">--%>
+                <%--<img src="../static/images/mainBG2.png" class="largeImage"/>--%>
+        <%--<th style="width: 20%">--%>
+            <%--夏季短袖--%>
+        <%--</th>--%>
+        <%--<th style="width: 20%">--%>
+            <%--130--%>
+        <%--</th>--%>
+        <%--<th style="width: 20%">--%>
+            <%--未卖出--%>
+        <%--</th>--%>
+    <%--</tr>--%>
+    <%--</tbody>--%>
+<%--</table>--%>
+<script type="text/javascript" src="../static/js/uploadedClothes.js"></script>
+
 </body>
 </html>
---
+++ src/main/webapp/static/js/uploadedClothes.js
@@ -0,0 +1,112 @@
+/**
+ * Created by island on 2017/8/11.
+ */
+function getAllClothes() {
+    var id = getCookie('id');
+    jQuery.ajax({
+        type: 'POST',
+        url: '/clothesAction/findClothesByUser',
+        data: {
+            "id": id,
+            "type": '1'
+        },
+        dataType: 'json',
+        success: function (data) {
+            // alert("success");
+            if (data && data.success == "true") {
+                if (data.clothes.length == 0) {
+                    $('#noClothesLabel').css("display", "none");
+                    $.each(data.clothes, function (i, item) {
+                        addSingleClothes(item.clothesID, data.pics[i], item.clothesType, item.clothessize, item.status);
+                    });
+                }
+            } else {
+                fail_alert("无结果");
+            }
+        },
+        error: function () {
+            fail_alert("哎呀呀，网络似乎不太好...")
+        }
+    });
+}
+
+function addSingleClothes(clothesID, picurl, type, size, status) {
+    var body = document.getElementById('body');
+    var table = document.createElement("table");
+    table.className = "table-content";
+    // table.style.top = (2 + 20*i) + '%';
+    body.appendChild(table);
+
+    var tbody1 = document.createElement("tbody");
+    tbody1.className = "table-content-title";
+    table.appendChild(tbody1);
+
+    var tr1 = document.createElement("tr");
+    tbody1.appendChild(tr1);
+
+
+    var td1 = document.createElement("td");
+    td1.style.width = "30%";
+    var a1 = document.createElement("a");
+    // a.href = "../jsp/clothesDetail?id=" + clothesID;
+    a1.onclick = function () {
+        window.open("../jsp/clothesDetails?id=" + clothesID);
+    };
+    a1.appendChild(document.createTextNode(clothesID));
+    td1.appendChild(a1);
+    tr1.appendChild(td1);
+
+    var td2 = document.createElement("td");
+    td2.style.width = "20%";
+    td2.appendChild(document.createTextNode("款式"));
+    tr1.appendChild(td2);
+
+    var td3 = document.createElement("td");
+    td3.style.width = "20%";
+    td3.appendChild(document.createTextNode("尺寸"));
+    tr1.appendChild(td3);
+
+    var td4 = document.createElement("td");
+    td4.style.width = "20%";
+    td4.appendChild(document.createTextNode("状态"));
+    tr1.appendChild(td4);
+
+    var tbody2 = document.createElement("tbody");
+    tbody2.className = "table-content-body";
+    table.appendChild(tbody2);
+
+    var tr2 = document.createElement("tr");
+    tbody2.appendChild(tr2);
+
+    var th2 = document.createElement("th");
+    th2.style.width = "30%";
+    var div = document.createElement("div");
+    div.className = 'imageBox';
+    th2.appendChild(div);
+    var img = document.createElement("img");
+    img.src = picurl;
+    img.className = 'largeImage';
+    div.appendChild(img);
+    tr2.appendChild(th2);
+
+    var th4 = document.createElement("th");
+    th4.style.width = "20%";
+    th4.appendChild(document.createTextNode(type));
+    tr2.appendChild(th4);
+
+    var th5 = document.createElement("th");
+    th4.style.width = "20%";
+    th4.appendChild(document.createTextNode(size));
+    tr2.appendChild(th4);
+
+
+    var th6 = document.createElement("th");
+    th4.style.width = "20%";
+    if (status == 'Available')
+        th4.appendChild(document.createTextNode("未卖出"));
+    else
+        th4.appendChild(document.createTextNode("已卖出"));
+    tr2.appendChild(th4);
+
+
+}
